6165f12c9daae0e710ccd37237460a819cda97c4
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-03-22 07:08:41 -0600

    Initial commit turn.rb

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+class Turn
+    class Turn
+
+        # Classes:
+        # Card
+        # --> Attributes:
+            # - question
+            # - answer
+            # - category
+        # Turn
+            # - card
+            # - guess
+            # - correct?
+            # - feedback
+        # Deck
+        # Round
+            # - deck
+            # - turns
+            # - current_card 
+            # - take_turn
+            # - class
+            # - correct?
+            # - number_correct
+            # - current_card
+            # - feedback
+            # - number_correct_by_category
+            # - percent_correct
+        
+        # Card_Generator
+        
