d6dd21413acdd1a26412185419aa135bbb418fac
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-03-23 15:14:30 -0600

    Add two tests to turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,4 +1,22 @@
 require './lib/card'
 require './lib/turn'
 
-# Initial commit
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)   
+    
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'returns the user guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card) 
+
+    expect(guess).to eq("Juneau")
+  end
+end
