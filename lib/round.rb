ce3d08e753d5aac31fe1f7155ef618c0bf33e3f4
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-03-26 17:52:21 -0600

    Show as far as I've gotten.

--- lib/round.rb
+++ lib/round.rb
@@ -8,16 +8,35 @@
 # take_turn stores the new Turn and return in from the take_turn method.
 # After take_turn is called the Round should move onto the next card in the deck.
 
+# How do I record a guess? Maybe by putting it into a new array??
+
+# Round begins with 1st element of cards array == current_card (card_1)
+    # A guess is made.
+        # That guess is recorded.
+            # Next element in cards array becomes current_card (card_2)
+
+            
+
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
+require 'pry'
 
 class Round
-    attr_reader :deck
+    attr_reader :deck,
+                :turns,
+                :current_card
 
     def initialize(deck)
         @deck = deck
+        @turns = []
+        @current_card = deck.cards[turns.count]
+        binding.pry
+                       # deck.cards[0] = card_1
+                        # Because card_1 is the element in the cards array at the 
+                            # 0th index position
     end
+
 end
 
                             # attr_reader :current_card,
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,20 +2,75 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
+require 'pry'
 
 RSpec.configure do |config|
     config.formatter = :documentation
 end
 
 RSpec.describe Round do
-    it 'exists' do
+    it 'exists and has a deck and turns' do
 
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
      
         cards = [card_1, card_2, card_3]
+    
+        turn = Turn.new("Juneau", cards)
         deck = Deck.new(cards)
         round = Round.new(deck)
+
+        expect(round).to be_instance_of(Round)
+        expect(round.deck).to eq(deck)
+        expect(round.turns).to eq([])
+
+    end
+
+    it 'starts a Round with current_card == 1st element in cards array (card_1)' do
+    
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+
+        expect(round.current_card).to eq(card_1)
+    end
+        
+    it 'has a turn' do
+
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+        
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        
+    end
+
+
+    it 'has a deck' do
+    
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    
+    expect(round.deck).to eq(deck)
+        # round.deck
+            # Is calling the deck method (via attr_reader) onto the variable `round`
+            # which holds the round object.
+            # `round` is initialized with the deck attribute
+            # deck holds the value of the object `deck`
     end
 end
